076915850d3897bfd08885412689bbc5fa1807ad
Raphael COUSSEMENT <raphael.coussement@diiage.org>
2025-09-10 08:12:39 +0200

    init

---
+++ movie-foyer/src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-blue">
+            <h1 className="text-4xl font-bold text-[#E53A0C] mb-6">MovieNight 🎬</h1>
+            <p>Bienvenue ! Ici vous pourrez voir et voter pour les films du samedi.</p>
+        </div>
+    );
+}
